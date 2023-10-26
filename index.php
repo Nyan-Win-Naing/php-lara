e0f52e7500c7ea10df6402d1b3bffe52e05a3403
Nyan-Win-Naing <nyanwinnaing1922002@gmail.com>
2023-10-26 13:09:42 +0630

    php code with html

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-    require "test_1.php";
-    echo "hi i am index";
+    $user = "Aung Aung";
+    $admin = "Nyan Win Naing";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>User name is <?php echo $user; ?></h1>
+
+    <?php
+        if($admin == "Nyan Win Naing") {
+    ?>
+    <h1>Admin name is <?php echo $admin; ?></h1>
+    <?php   
+    }
+    ?>
+
+
+
+</body>
+</html>
